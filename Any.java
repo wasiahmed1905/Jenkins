5dc22ca7413822442c35a701704eaa66fc8f6076
wasiahmed1905 <wasiahmed19052gmail.com>
2026-09-24 14:59:58 +0000

    Java

---
+++ Any.java
@@ -0,0 +1,18 @@
+public class Any {
+
+    public static void main(String[] args) {
+        // Output a simple greeting to verify the code works
+        System.out.println("Hello from the Any class!");
+
+        // Example: Demonstrating a basic task
+        int result = addNumbers(5, 10);
+        System.out.println("Result of 5 + 10 = " + result);
+    }
+
+    /**
+     * A helper method to perform addition.
+     */
+    public static int addNumbers(int a, int b) {
+        return a + b;
+    }
+}
